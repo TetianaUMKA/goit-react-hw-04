1863b79cd7d673089411f677f5fa564c20427b97
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2024-09-02 14:03:04 +0300

    Install modern-normalize

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
+import "modern-normalize";
+
+import App from "./components/App/App";
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
