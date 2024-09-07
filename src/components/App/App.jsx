d489a616e03409db68ead8150ec034fd3f124243
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2024-09-07 21:14:26 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,40 @@
-// import { useState } from "react";
+import { useState, useEffect } from "react";
+import SearchBar from "../SearchBar/SearchBar";
+import ImageGallery from "../ImageGallery/ImageGallery";
+import { fetchPhotos } from "../../api";
 
 function App() {
-  return <></>;
+  const [query, setQuery] = useState("");
+  const [gallery, setGallery] = useState([]);
+  const [page, setPage] = useState(1);
+
+  const handleSearch = (newQuery) => {
+    setQuery(newQuery);
+    console.log(query);
+  };
+
+  useEffect(() => {
+    if (!query) {
+      return;
+    }
+    async function getImages() {
+      try {
+        const fetchedPhotos = await fetchPhotos(query, page);
+        setGallery(fetchedPhotos);
+        console.log(fetchedPhotos);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getImages();
+  }, [query, page]);
+
+  return (
+    <>
+      <SearchBar onSearch={handleSearch} />
+      {gallery.length > 0 && <ImageGallery images={gallery} />}
+    </>
+  );
 }
 
 export default App;
